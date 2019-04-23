e170f7e15afcb0dfaf49546db34f3d0c0bcb143e
qianlihaoyue <qianlihaoyue@gmail.com>
2019-04-23 20:27:47 +0800

    Site updated: 2019-04-23 20:27:46

---
+++ js/me/text.js
@@ -0,0 +1,33 @@
+<!script type="text/javascript" src="js/jquery.min.js"></script>
+
+
+<script>
+/* 鼠标特效 */
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array("c","c++","python","Java","PHP","Go","R","Js"");
+        var $i = $("<span></span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+        1500,
+        function() {
+            $i.remove();
+        });
+    });
+});
+</script>
